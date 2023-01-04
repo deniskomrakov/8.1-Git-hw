b4a2885a8d68fbf72bd722550efe7ac52854bc25
Denis Komrakov <deniskomrakov@gmail.com>
2023-01-04 17:24:04 +0300

    dev, initial comment

---
+++ case.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Enter the file's name with an extention: "
+
+read a
+
+case "$a" in
+        *.jpd|gif|png)
+        echo "This is an image"
+        ;;
+        *.mp3|wav)
+        echo "This is an audio"
+        ;;
+        txt|doc)
+        echo "This is a text"
+        ;;
+        *)
+        echo "Unknown"
+
+esac
+
